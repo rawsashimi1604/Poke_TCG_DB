217463d551e8db4ab5ecc6cfd367eac16c288f4f
Gavin Loo <looweiren@gmail.com>
2022-08-24 00:56:19 +0800

    Added pokemon TCG SDK

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,11 @@ import pokemon from "pokemontcgsdk";
 const app = express();
 const PORT = 3000;
 
+app.get("/", async (req, res) => {
+  const data = await pokemon.set.all()
+  res.send(data);
+});
+
 app.listen(PORT, (e) => {
   if (e) {
     console.log("Error occured, cannot start server...", e);
