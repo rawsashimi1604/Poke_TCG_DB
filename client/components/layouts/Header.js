7a0d618083235451f1297cb1082fc732b95260e3
Gavin Loo <looweiren@gmail.com>
2022-08-31 02:00:37 +0800

    added som responsiveness to landing page

--- client/components/layouts/Header.js
+++ client/components/layouts/Header.js
@@ -6,16 +6,16 @@ import LandingCard from "../components/LandingCard";
 
 function Header() {
   return (
-    <section className="h-72 w-full border-b-2 border-gray-300 mb-4">
-      <div className="grid grid-cols-10 gap-4">
-        <div className="col-span-6">
+    <section className="xl:h-72 w-full border-b-2 border-gray-300 mb-4">
+      <div className="grid grid-cols-6 xl:grid-cols-10 gap-4">
+        <div className="col-span-6 xl:col-span-6">
           <div>
             <h1 className="font-semibold text-xl mb-2">Statistics</h1>
             <LandingCard />
           </div>
         </div>
 
-        <div className="col-span-4 mb-2">
+        <div className="col-span-6 xl:col-span-4 mb-2">
           <h1 className="font-semibold text-xl mb-2">Info</h1>
           <InfoCard />
         </div>
