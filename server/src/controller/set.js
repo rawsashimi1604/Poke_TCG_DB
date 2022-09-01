62b458cebb13c5a0ac53dbdcf00905d1a289c23d
Gavin Loo <looweiren@gmail.com>
2022-09-01 17:55:19 +0800

    added set controller

---
+++ server/src/controller/set.js
@@ -0,0 +1,28 @@
+import { SetQuery } from "../db/queries/set.js";
+
+function handleIndex(req, res) {
+  res.send("Set route...");
+}
+
+async function handleAllCardSets(req, res) {
+  const queryRes = await SetQuery.getAllSets();
+  res.send(queryRes.rows);
+}
+
+async function handleCardSetCount(req, res) {
+  const queryRes = await SetQuery.getSetQuantity();
+  res.send(queryRes.rows[0]);
+}
+
+async function handleSpecificCardSetBySetId(req, res) {
+  const id = req.params.id;
+  const queryRes = await SetQuery.getSetById(id);
+  res.send(queryRes.rows);
+}
+
+export default {
+  handleIndex,
+  handleAllCardSets,
+  handleCardSetCount,
+  handleSpecificCardSetBySetId,
+};
--- server/src/routes/set.js
+++ server/src/routes/set.js
@@ -1,4 +1,5 @@
 import express from "express";
+import SetController from "../controller/set.js";
 import { SetQuery } from "../db/queries/index.js";
 
 const router = express.Router();
@@ -9,27 +10,9 @@ router.use((req, res, next) => {
 });
 
 // Routes
-router.get("/", (req, res) => {
-  res.send("Set route...");
-});
-
-// Get all card_sets...
-router.get("/all", async (req, res) => {
-  const queryRes = await SetQuery.getAllSets();
-  res.send(queryRes.rows);
-});
-
-// Get number of sets
-router.get("/count", async (req, res) => {
-  const queryRes = await SetQuery.getSetQuantity();
-  res.send(queryRes.rows[0]);
-});
-
-// Get specific card_set...
-router.get("/id/:id", async (req, res) => {
-  const id = req.params.id;
-  const queryRes = await SetQuery.getSetById(id);
-  res.send(queryRes.rows);
-});
+router.get("/", SetController.handleIndex);
+router.get("/all", SetController.handleAllCardSets);
+router.get("/count", SetController.handleCardSetCount);
+router.get("/id/:id", SetController.handleSpecificCardSetBySetId);
 
 export default router;
