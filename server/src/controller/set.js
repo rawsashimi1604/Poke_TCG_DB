4102265ad8b91928230edb7c49c120e2d0ea1f15
Gavin Loo <looweiren@gmail.com>
2022-09-01 21:38:28 +0800

    refactored set controller

--- server/src/controller/set.js
+++ server/src/controller/set.js
@@ -6,7 +6,7 @@ function handleIndex(req, res) {
 
 async function handleAllCardSets(req, res) {
   const queryRes = await SetQuery.getAllSets();
-  res.send(queryRes.rows);
+  res.send(await Promise.all(queryRes.rows.map((set) => set.data)));
 }
 
 async function handleCardSetCount(req, res) {
@@ -17,7 +17,7 @@ async function handleCardSetCount(req, res) {
 async function handleSpecificCardSetBySetId(req, res) {
   const id = req.params.id;
   const queryRes = await SetQuery.getSetById(id);
-  res.send(queryRes.rows);
+  res.send(queryRes.rows[0].data);
 }
 
 export default {
