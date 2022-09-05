e3ab4263f4d60aaed634464de4ed4ceeb612c272
Gavin Loo <looweiren@gmail.com>
2022-09-06 01:15:39 +0800

    refactored .env file

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,7 +1,13 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+console.log(process.env.PG_PASSWORD);
 
 const devConfig = {
   user: process.env.PG_USER,
--- src/scripts/card.js
+++ src/scripts/card.js
@@ -4,8 +4,12 @@
 import dotenv from "dotenv";
 import db from "../dbConfig.js";
 import pokemon from "pokemontcgsdk";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 function sleep(ms) {
   return new Promise((resolve) => {
