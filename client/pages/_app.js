ddd82a349e52cbe9d2526a2237c13d55ebf193ff
Gavin Loo <looweiren@gmail.com>
2022-09-06 17:24:40 +0800

    added more favicons

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,6 +9,9 @@ function MyApp({ Component, pageProps }) {
           Poké TCG DB - Your one stop location for Pokémon TCG Cards!
         </title>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </Head>
       <Component {...pageProps} />
     </>
