c57d41a61053807534d2e5a58fa32e654ea75714
Gavin Loo <looweiren@gmail.com>
2022-09-06 17:28:05 +0800

    added favicon absolute URLs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -8,10 +8,10 @@ function MyApp({ Component, pageProps }) {
         <title>
           Poké TCG DB - Your one stop location for Pokémon TCG Cards!
         </title>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="shortcut icon" href="https://raw.githubusercontent.com/rawsashimi1604/Poke_TCG_DB/master/client/public/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://github.com/rawsashimi1604/Poke_TCG_DB/blob/master/client/public/apple-touch-icon.png?raw=true" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://github.com/rawsashimi1604/Poke_TCG_DB/blob/master/client/public/favicon-32x32.png?raw=true"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://github.com/rawsashimi1604/Poke_TCG_DB/blob/master/client/public/favicon-16x16.png?raw=true"/>
       </Head>
       <Component {...pageProps} />
     </>
